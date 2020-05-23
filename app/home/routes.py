b1467379e9ece3007d266ec1e019cc7caff03997
MohmedH <mohmedhira@yahoo.com>
2020-05-23 09:07:44 -0500

    LOTS OF CHANGES
    Brand new Table style with animated buttons, changed logic in how the users are being saved, basically using id from db and assuming an admin wouldn;t want to mess with this.

--- app/home/routes.py
+++ app/home/routes.py
@@ -39,32 +39,17 @@ def index():
 
     return render_template('index.html', chart = dataDailySalesChart, investAMT=investmentAmt)
 
-@blueprint.route('/profiles', methods=['GET', 'POST', 'PUT'])
+@blueprint.route('/profiles', methods=['GET'])
 @login_required
 def profile():
-    if  current_user.role != 'admin':
-        return render_template('page-403.html'), 403
-    else:
-        if request.method == 'POST' or request.method == 'PUT':
-            content = request.get_json()
-            print(content)
-            '''
-            print(request.form.keys())
-            test = request.form
-            print(request.form.get('moe[age]'))
-            for t in test:
-                print(t)
-            '''
-        
-        users = User.query.all()
-        temp = {}
-        for user in users:
-            temp[user] = {
-                'username':user,
-                'email':user.email,
-                'role':user.role,
-            }
-        return render_template('profiles.html', test=temp)
+    try:
+        if  current_user.role != 'admin':
+            return render_template('page-403.html'), 403
+        else:
+            users = User.query.all()
+            return render_template('profiles.html', test=users)
+    except:
+        return render_template('page-500.html'), 500
 
 @blueprint.route('/profiles/<action>', methods=['POST', 'PUT', 'DELETE'])
 @login_required
@@ -74,46 +59,76 @@ def profileActions(action):
 
     if request.method == 'POST' or request.method == 'PUT':
         content = request.get_json()
-        print(content)
+        #print(content)
 
-        user = User.query.filter_by(username=content['old']['username']).first()
+        try:
+            user = User.query.filter_by(id=content['id']).first()
 
-        if user:
-            #If I query the new username and find it, and it isnt the same as your old, fail 
-            if User.query.filter_by(username=content['new']['username']).first() and content['old']['username'] != content['new']['username']:
-                return json.dumps({'save':'failed'}), 401, {'ContentType':'application/json'}
+            if user:
+                if User.query.filter_by(username=content['username']).first() and user.username != content['username']:
+                    return json.dumps({'save':'failed'}), 401, {'ContentType':'application/json'}
 
-            if User.query.filter_by(email=content['new']['email']).first() and content['old']['email'] != content['new']['email']:
-                return json.dumps({'save':'failed'}), 401, {'ContentType':'application/json'}
+                if User.query.filter_by(username=content['email']).first() and user.email != content['email']:
+                    return json.dumps({'save':'failed'}), 401, {'ContentType':'application/json'}
 
-            port = Portfolio.query.filter_by(email=content['old']['email']).first()
-            user.username = content['new']['username']
-            user.email = content['new']['email']
-            port.email = content['new']['email']
-            db.session.commit() 
-        else:
-            userEmailCheck = User.query.filter_by(email=content['new']['email']).first()
-            if userEmailCheck:
-                return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
+                port = Portfolio.query.filter_by(email=user.email).first()
+                user.username = content['username']
+                user.email = content['email']
+                port.email = content['email']
+                db.session.commit()
+
+
             else:
+                userEmailCheck = User.query.filter_by(email=content['email']).first()
+                if userEmailCheck:
+                    return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
+
+                userNameCheck = User.query.filter_by(username=content['username']).first()
+                if userNameCheck:
+                    return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
+
                 userNew = User()
-                userNew.username = content['new']['username']
-                userNew.email = content['new']['email']
+                userNew.username = content['username']
+                userNew.email = content['email']
                 pwd = hash_pass( 'pass' )
                 userNew.password = pwd
 
                 portNew = Portfolio()
-                portNew.email = content['new']['email']
+                portNew.email = content['email']
                 portNew.invested = 0
 
                 db.session.add(userNew)
                 db.session.add(portNew)
                 db.session.commit()
 
-        #Add the user or update the user then send 200, 
-        #return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
-        return json.dumps({'save':'success'}), 200, {'ContentType':'application/json'}
+            #Add the user or update the user then send 200, 
+            #return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
+            return json.dumps({'save':'success'}), 200, {'ContentType':'application/json'}
+        except:
+            return json.dumps({'save':'failed'}), 400, {'ContentType':'application/json'}
+    
+    if request.method == 'DELETE':
+        try:
+            content = request.get_json()
+            #print(content)
+            if content['id'] == '1':
+                return json.dumps({'save':'failed'}), 404, {'ContentType':'application/json'}
+            else:
+                
+                count = User.query.filter_by(id=content['id']).count()
+                if count != 0:
+                    user = User.query.filter_by(id=content['id']).one()
+                    port = Portfolio.query.filter_by(email=user.email).one()
+                    db.session.delete(user)
+                    db.session.delete(port)
+                    db.session.commit()
+                    return json.dumps({'save':'success'}), 200, {'ContentType':'application/json'}
+                else:
+                    return json.dumps({'save':'success'}), 200, {'ContentType':'application/json'}
             
+
+        except:
+            return json.dumps({'save':'failed'}), 400, {'ContentType':'application/json'}
 
 @blueprint.route('/test', methods=['GET'])
 def test():
@@ -140,81 +155,85 @@ def page_user(username):
     if username != current_user.username:
         return render_template('page-403.html'), 403
 
-    #Use the bottom after a sussessful password reset or any message for that matter
-    #return render_template('profile.html', passmsg="Success")
-    pass_form = ChangePassword(request.form)
-    profile_form = UpdateProfile(request.form)
-    
-    #If the username is not unique this falls apart as anyone can change username to match and then change that password.
-    if 'changePass' in request.form:
-        print(request.form)
-        user = User.query.filter_by(username=current_user.username).first()
-        passwordOld = request.form['passwordOld']
-        passwordNew = request.form['passwordNew']
-        passwordVerify = request.form['passwordVerify']
-        if user and verify_pass( passwordOld, user.password):
-            if passwordNew == passwordVerify:
-                #Change the password here!
-                pwd = hash_pass( passwordNew )
-                user.password = pwd
+    try:
+        #Use the bottom after a sussessful password reset or any message for that matter
+        #return render_template('profile.html', passmsg="Success")
+        pass_form = ChangePassword(request.form)
+        profile_form = UpdateProfile(request.form)
+        
+        #If the username is not unique this falls apart as anyone can change username to match and then change that password.
+        if 'changePass' in request.form:
+            print(request.form)
+            user = User.query.filter_by(username=current_user.username).first()
+            passwordOld = request.form['passwordOld']
+            passwordNew = request.form['passwordNew']
+            passwordVerify = request.form['passwordVerify']
+            if user and verify_pass( passwordOld, user.password):
+                if passwordNew == passwordVerify:
+                    #Change the password here!
+                    pwd = hash_pass( passwordNew )
+                    user.password = pwd
+                    db.session.commit()
+                    passmsgg = 'Successfully Changed Password!'
+                else:
+                    passmsgg = 'error New passwords did not match!'
+            else:
+                passmsgg = 'error please make sure you entered the current password properly'
+
+            return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, passmsg=passmsgg)
+        #This is for updating information to the profile, **!!IF EMAIL CHANGES YOU MUST CHANGE IN ALL TABLES!!**
+        elif 'updateProfile' in request.form:
+            print(request.form)
+            username  = request.form['username']
+            email     = request.form['email'   ]
+            
+            #First Check make sure the user name in the form is different from current, meaning they want to change it
+            if username != current_user.username:
+                #Now Query that user and make sure that it does not already exist
+                user = User.query.filter_by(username=username).first()
+                if user:
+                    return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Error User already exists')
+                else:
+                    #If you get here you will be able to change that username, because it does not exist, and it is not the same as current
+                    changeUser = True
+            else:
+                changeUser = False
+
+
+            if email != current_user.email:   
+                user = User.query.filter_by(email=email).first()
+                if user:
+                    return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Error Email already exists')
+                else:
+                    changeEmail = True
+            else:
+                changeEmail = False
+
+            #UPDATE THE INFO HERE
+            user = User.query.filter_by(username=current_user.username).first()
+
+            #Change username in USER table
+            if changeUser:
+                user.username = username
+            
+            #Change email in USER and PORTFOLIO tables
+            if changeEmail:
+                portfolio = Portfolio.query.filter_by(email=current_user.email).first()
+                user.email = email
+                portfolio.email = email
+        
+            if changeEmail or changeUser:
+                #Commit the changes!
                 db.session.commit()
-                passmsgg = 'Successfully Changed Password!'
+                return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Changed Info Successfully!')
             else:
-                passmsgg = 'error New passwords did not match!'
+                return render_template('profile.html', formpassword=pass_form, formprofile=profile_form)
         else:
-            passmsgg = 'error please make sure you entered the current password properly'
-
-        return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, passmsg=passmsgg)
-    #This is for updating information to the profile, **!!IF EMAIL CHANGES YOU MUST CHANGE IN ALL TABLES!!**
-    elif 'updateProfile' in request.form:
-        print(request.form)
-        username  = request.form['username']
-        email     = request.form['email'   ]
-        
-        #First Check make sure the user name in the form is different from current, meaning they want to change it
-        if username != current_user.username:
-            #Now Query that user and make sure that it does not already exist
-            user = User.query.filter_by(username=username).first()
-            if user:
-                return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Error User already exists')
-            else:
-                #If you get here you will be able to change that username, because it does not exist, and it is not the same as current
-                changeUser = True
-        else:
-            changeUser = False
-
-
-        if email != current_user.email:   
-            user = User.query.filter_by(email=email).first()
-            if user:
-                return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Error Email already exists')
-            else:
-                changeEmail = True
-        else:
-            changeEmail = False
-
-        #UPDATE THE INFO HERE
-        user = User.query.filter_by(username=current_user.username).first()
-
-        #Change username in USER table
-        if changeUser:
-            user.username = username
-        
-        #Change email in USER and PORTFOLIO tables
-        if changeEmail:
-            portfolio = Portfolio.query.filter_by(email=current_user.email).first()
-            user.email = email
-            portfolio.email = email
-    
-        if changeEmail or changeUser:
-            #Commit the changes!
-            db.session.commit()
-            return render_template('profile.html', formpassword=pass_form, formprofile=profile_form, profilemsg='Changed Info Successfully!')
-        else:
+            #Default page render, just want to see what's going on
             return render_template('profile.html', formpassword=pass_form, formprofile=profile_form)
-    else:
-        #Default page render, just want to see what's going on
-        return render_template('profile.html', formpassword=pass_form, formprofile=profile_form)
+    except:
+        return render_template('page-500.html'), 500
+
 
 
 @blueprint.route('/<template>')
